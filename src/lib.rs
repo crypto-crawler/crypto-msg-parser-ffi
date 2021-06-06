e624f1a6de9876536defbcc04a0b385f12363cc7
soulmachine <soulmachine@gmail.com>
2021-06-05 17:22:37 -0700

    First version

---
+++ src/lib.rs
@@ -0,0 +1,224 @@
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
+mod market_type;
+pub use market_type::*;
+
+use std::{
+    ffi::{CStr, CString},
+    os::raw::c_char,
+};
+
+/// Parse a raw trade message into a Vec<TradeMsg> and then convert to a JSON string.
+#[no_mangle]
+pub extern "C" fn parse_trade(
+    exchange: *const c_char,
+    market_type: MarketType,
+    msg: *const c_char,
+) -> *const c_char {
+    let exchange_c_str = unsafe {
+        debug_assert!(!exchange.is_null());
+        CStr::from_ptr(exchange)
+    };
+    let exchange = exchange_c_str.to_str().unwrap();
+
+    let market_type = market_type.to_rust();
+
+    let msg_c_str = unsafe {
+        debug_assert!(!msg.is_null());
+        CStr::from_ptr(msg)
+    };
+    let msg = msg_c_str.to_str().unwrap();
+
+    if let Ok(trades) = crypto_msg_parser::parse_trade(exchange, market_type, msg) {
+        let text = serde_json::to_string(&trades).unwrap();
+        let raw = CString::new(text).unwrap();
+        raw.into_raw() as *const c_char
+    } else {
+        std::ptr::null()
+    }
+}
+
+/// Parse a raw level2 orderbook message into a Vec<OrderBookMsg> and then convert to a JSON string.
+#[no_mangle]
+pub extern "C" fn parse_l2(
+    exchange: *const c_char,
+    market_type: MarketType,
+    msg: *const c_char,
+) -> *const c_char {
+    let exchange_c_str = unsafe {
+        debug_assert!(!exchange.is_null());
+        CStr::from_ptr(exchange)
+    };
+    let exchange = exchange_c_str.to_str().unwrap();
+
+    let market_type = market_type.to_rust();
+
+    let msg_c_str = unsafe {
+        debug_assert!(!msg.is_null());
+        CStr::from_ptr(msg)
+    };
+    let msg = msg_c_str.to_str().unwrap();
+
+    if let Ok(orderbooks) = crypto_msg_parser::parse_l2(exchange, market_type, msg) {
+        let text = serde_json::to_string(&orderbooks).unwrap();
+        let raw = CString::new(text).unwrap();
+        raw.into_raw() as *const c_char
+    } else {
+        std::ptr::null()
+    }
+}
+
+/// Parse a raw funding rate message into a Vec<FundingRateMsg> and then convert to a JSON string.
+#[no_mangle]
+pub extern "C" fn parse_funding_rate(
+    exchange: *const c_char,
+    market_type: MarketType,
+    msg: *const c_char,
+) -> *const c_char {
+    let exchange_c_str = unsafe {
+        debug_assert!(!exchange.is_null());
+        CStr::from_ptr(exchange)
+    };
+    let exchange = exchange_c_str.to_str().unwrap();
+
+    let market_type = market_type.to_rust();
+
+    let msg_c_str = unsafe {
+        debug_assert!(!msg.is_null());
+        CStr::from_ptr(msg)
+    };
+    let msg = msg_c_str.to_str().unwrap();
+
+    if let Ok(rates) = crypto_msg_parser::parse_funding_rate(exchange, market_type, msg) {
+        let text = serde_json::to_string(&rates).unwrap();
+        let raw = CString::new(text).unwrap();
+        raw.into_raw() as *const c_char
+    } else {
+        std::ptr::null()
+    }
+}
+
+/// Deallocate a string.
+#[no_mangle]
+pub extern "C" fn deallocate_string(pointer: *const c_char) {
+    unsafe {
+        if pointer.is_null() {
+            return;
+        }
+        CString::from_raw(pointer as *mut c_char)
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::MarketType;
+
+    use super::{deallocate_string, parse_funding_rate, parse_l2, parse_trade};
+    use float_cmp::approx_eq;
+    use std::ffi::{CStr, CString};
+
+    #[test]
+    fn test_parse_trade() {
+        let (json_ptr, json_str) = {
+            let exchange = CString::new("binance").unwrap();
+            let raw_msg = CString::new(r#"{"stream":"btcusd_perp@aggTrade","data":{"e":"aggTrade","E":1616201883458,"a":41045788,"s":"BTCUSD_PERP","p":"58570.1","q":"58","f":91864326,"l":91864327,"T":1616201883304,"m":true}}"#).unwrap();
+
+            let json_ptr =
+                parse_trade(exchange.as_ptr(), MarketType::InverseSwap, raw_msg.as_ptr());
+            let json_c_str = unsafe {
+                debug_assert!(!json_ptr.is_null());
+                CStr::from_ptr(json_ptr)
+            };
+
+            (json_ptr, json_c_str.to_str().unwrap())
+        };
+
+        let trades = serde_json::from_str::<Vec<crypto_msg_parser::TradeMsg>>(json_str).unwrap();
+        assert_eq!(trades.len(), 1);
+        let trade = &trades[0];
+
+        assert_eq!(trade.exchange, "binance");
+        assert_eq!(
+            trade.market_type,
+            crypto_msg_parser::MarketType::InverseSwap
+        );
+        assert_eq!(trade.price, 58570.1);
+        assert!(approx_eq!(
+            f64,
+            trade.quantity_base,
+            5800.0 / 58570.1,
+            epsilon = 0.00000000000000002
+        ));
+        assert_eq!(trade.quantity_quote, 5800.0);
+        assert_eq!(trade.quantity_contract, Some(58.0));
+        assert_eq!(trade.side, crypto_msg_parser::TradeSide::Sell);
+
+        deallocate_string(json_ptr);
+    }
+
+    #[test]
+    fn test_parse_l2() {
+        let (json_ptr, json_str) = {
+            let exchange = CString::new("binance").unwrap();
+            let raw_msg = CString::new(r#"{"stream":"btcusd_perp@depth@100ms","data":{"e":"depthUpdate","E":1622370862564,"T":1622370862553,"s":"BTCUSD_PERP","ps":"BTCUSD","U":127559587191,"u":127559588177,"pu":127559587113,"b":[["35365.9","1400"],["35425.8","561"]],"a":[["35817.8","7885"],["35818.7","307"]]}}"#).unwrap();
+
+            let json_ptr = parse_l2(exchange.as_ptr(), MarketType::InverseSwap, raw_msg.as_ptr());
+            let json_c_str = unsafe {
+                debug_assert!(!json_ptr.is_null());
+                CStr::from_ptr(json_ptr)
+            };
+
+            (json_ptr, json_c_str.to_str().unwrap())
+        };
+
+        let orderbooks =
+            serde_json::from_str::<Vec<crypto_msg_parser::OrderBookMsg>>(json_str).unwrap();
+        assert_eq!(orderbooks.len(), 1);
+        let orderbook = &orderbooks[0];
+
+        assert_eq!(orderbook.exchange, "binance");
+        assert_eq!(
+            orderbook.market_type,
+            crypto_msg_parser::MarketType::InverseSwap
+        );
+        assert_eq!(orderbook.asks.len(), 2);
+        assert_eq!(orderbook.bids.len(), 2);
+        assert!(!orderbook.snapshot);
+        assert_eq!(orderbook.timestamp, 1622370862553);
+
+        assert_eq!(orderbook.bids[0].price, 35365.9);
+
+        assert_eq!(orderbook.asks[0].price, 35817.8);
+
+        deallocate_string(json_ptr);
+    }
+
+    #[test]
+    fn test_parse_funding_rate() {
+        let (json_ptr, json_str) = {
+            let exchange = CString::new("binance").unwrap();
+            let raw_msg = CString::new(r#"{"stream":"btcusd_perp@markPrice","data":{"e":"markPriceUpdate","E":1617309477000,"s":"BTCUSD_PERP","p":"59012.56007222","P":"58896.00503145","r":"0.00073689","T":1617321600000}}"#).unwrap();
+
+            let json_ptr =
+                parse_funding_rate(exchange.as_ptr(), MarketType::InverseSwap, raw_msg.as_ptr());
+            let json_c_str = unsafe {
+                debug_assert!(!json_ptr.is_null());
+                CStr::from_ptr(json_ptr)
+            };
+
+            (json_ptr, json_c_str.to_str().unwrap())
+        };
+
+        let rates =
+            serde_json::from_str::<Vec<crypto_msg_parser::FundingRateMsg>>(json_str).unwrap();
+        assert_eq!(rates.len(), 1);
+        let rate = &rates[0];
+
+        assert_eq!(rate.exchange, "binance");
+        assert_eq!(rate.market_type, crypto_msg_parser::MarketType::InverseSwap);
+        assert_eq!(rate.pair, "BTC/USD".to_string());
+        assert_eq!(rate.funding_rate, 0.00073689);
+        assert_eq!(rate.funding_time, 1617321600000);
+
+        deallocate_string(json_ptr);
+    }
+}
---
+++ src/market_type.rs
@@ -0,0 +1,56 @@
+/// Market type.
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub enum MarketType {
+    Spot,
+    LinearFuture,
+    InverseFuture,
+    LinearSwap,
+    InverseSwap,
+
+    AmericanOption,
+    EuropeanOption,
+
+    QuantoFuture,
+    QuantoSwap,
+
+    Move,
+    #[allow(clippy::upper_case_acronyms)]
+    BVOL,
+}
+
+impl MarketType {
+    // Converts C MarketType to Rust MarketType
+    pub fn to_rust(&self) -> crypto_msg_parser::MarketType {
+        match *self {
+            MarketType::Spot => crypto_msg_parser::MarketType::Spot,
+            MarketType::LinearFuture => crypto_msg_parser::MarketType::LinearFuture,
+            MarketType::InverseFuture => crypto_msg_parser::MarketType::InverseFuture,
+            MarketType::LinearSwap => crypto_msg_parser::MarketType::LinearSwap,
+            MarketType::InverseSwap => crypto_msg_parser::MarketType::InverseSwap,
+            MarketType::AmericanOption => crypto_msg_parser::MarketType::AmericanOption,
+            MarketType::EuropeanOption => crypto_msg_parser::MarketType::EuropeanOption,
+            MarketType::QuantoFuture => crypto_msg_parser::MarketType::QuantoFuture,
+            MarketType::QuantoSwap => crypto_msg_parser::MarketType::QuantoSwap,
+            MarketType::Move => crypto_msg_parser::MarketType::Move,
+            MarketType::BVOL => crypto_msg_parser::MarketType::BVOL,
+        }
+    }
+
+    // Converts Rust MarketType to C MarketType
+    pub fn from_rust(market_type: crypto_msg_parser::MarketType) -> Self {
+        match market_type {
+            crypto_msg_parser::MarketType::Spot => MarketType::Spot,
+            crypto_msg_parser::MarketType::LinearFuture => MarketType::LinearFuture,
+            crypto_msg_parser::MarketType::InverseFuture => MarketType::InverseFuture,
+            crypto_msg_parser::MarketType::LinearSwap => MarketType::LinearSwap,
+            crypto_msg_parser::MarketType::InverseSwap => MarketType::InverseSwap,
+            crypto_msg_parser::MarketType::AmericanOption => MarketType::AmericanOption,
+            crypto_msg_parser::MarketType::EuropeanOption => MarketType::EuropeanOption,
+            crypto_msg_parser::MarketType::QuantoFuture => MarketType::QuantoFuture,
+            crypto_msg_parser::MarketType::QuantoSwap => MarketType::QuantoSwap,
+            crypto_msg_parser::MarketType::Move => MarketType::Move,
+            crypto_msg_parser::MarketType::BVOL => MarketType::BVOL,
+        }
+    }
+}
